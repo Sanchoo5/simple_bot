3f7b5c274f8d8b1c80fbfc63cb4e6824fadecf34
Sanchoo5 <tursunov20010508@gmail.com>
2022-03-21 21:55:26 +0500

    Add logging username

--- bot.py
+++ bot.py
@@ -14,22 +14,11 @@ p.s. Санчо мой создатель  """.format(update.message.chat.first_
     logging.info('User {} press /start'.format(update.message.chat.username))
     update.message.reply_text(mytext)
 
-
-def name(update: Updater):
-    update.message.chat.first_name
-
-def reply(update: Updater):
-    text = update.message.text
-    update.message.reply_text(text)
-
 def chat(update: Updater, context: CallbackContext):
     text = update.message.text
     logging.info(text)
 
-    if name == 'Санчо':
-        reply + 'Ты прекрасен Санчо'
-    else:
-        reply()
+    update.message.reply_text(text)
 
 def main():
     updtr = Updater(settings.TOKEN_TG)
